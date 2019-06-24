9e1d4cce246a7cf6dd5cd534af91e314c2d2bee0
meislisha <838896349@qq.com>
2019-06-24 20:47:50 +0800

    歌手详情页基本显示

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,36 +1,80 @@
 <template>
     <transition name="slider">
-        <div class="singer-detail"></div>
+        <!-- <div class="singer-detail"></div> -->
+        <music-list :songs='songs' :title="title" :bg-image="bgImage"></music-list>
     </transition>
 </template>
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
+import { getSingerDetail } from "api/singer";
+import { ERR_OK } from "api/config";
+import { createSong } from "common/js/song";
+import MusicList from "components/music-list/music-list";
 export default {
-    computed: {
-        ...mapGetters([
-            'singer'
-        ])
+  data() {
+    return {
+      songs: []
+    };
+  },
+  computed: {
+    title() {
+      return this.singer.name;
     },
-    created(){
-        console.log(this.singer,1111111111111111111 )
+    bgImage() {
+      return this.singer.avatar;
+    },
+    ...mapGetters(["singer"])
+  },
+  components: { MusicList },
+  created() {
+    console.log(this.singer, 1111111111111111111);
+    this._getDetail();
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push("/singer");
+        return;
+      } //如果在歌手详情页不小心刷新页面，拿不到singer所以返回歌手页面
+      getSingerDetail(this.singer.id).then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.list);
+          this.songs = this._normalizeSongs(res.data.list);
+          console.log(this.songs);
+        }
+      });
+    },
+    _normalizeSongs(list) {
+      let ret = [];
+      list.forEach(item => {
+        let { musicData } = item;
+        if (musicData.songid && musicData.albumid) {
+          ret.push(createSong(musicData));
+          console.log(createSong(musicData) )
+        }
+      });
+      return ret;
     }
+  }
 };
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus">
 @import '~common/stylus/variable';
 
-.singer-detail {
-    position: fixed;
-    z-index: 100;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: $color-background;
+// .singer-detail {
+// position: fixed;
+// z-index: 100;
+// top: 0;
+// left: 0;
+// right: 0;
+// bottom: 0;
+// background: $color-background;
+// }
+.slide-enter-active, .slide-leave-active {
+    transition: all 0.3s;
 }
-.slide-enter-active, .slide-leave-active
-    transition: all 0.3s
 
-  .slide-enter, .slide-leave-to
-    transform: translate3d(100%, 0, 0)
+.slide-enter, .slide-leave-to {
+    transform: translate3d(100%, 0, 0);
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Recommend from 'components/recommend/recommend'
+// import Recommend from 'components/recommend/recommend'
 import Singer from 'components/singer/singer'
 import Search from 'components/search/search'
 import Rank from 'components/rank/rank'
 import SingerDetail from 'components/singer-detail/singer-detail'
 Vue.use(Router)
 
+// const Recommend = (resolve) => {
+//     import('components/recommend/recommend').then((module) => {
+//         resolve(module)
+//       })
+//   }
+    
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module)
+  })
+}
 export default new Router({
   routes: [
     {
